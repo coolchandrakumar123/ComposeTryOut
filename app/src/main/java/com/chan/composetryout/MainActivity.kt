24a3fe3676912af605e0cb7f9b27f4b8123cc1a3
chandra-1765 <chandrakumar.selvaraj@zohocorp.com>
2020-06-09 10:18:20 +0530

    compose initial

--- app/src/main/java/com/chan/composetryout/MainActivity.kt
+++ app/src/main/java/com/chan/composetryout/MainActivity.kt
@@ -2,10 +2,15 @@ package com.chan.composetryout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.ui.core.setContent
+import com.chan.composetryout.compose.inflateAndroidView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        setContent {
+            inflateAndroidView()
+        }
     }
 }
---
+++ app/src/main/java/com/chan/composetryout/compose/AndroidViewScreen.kt
@@ -0,0 +1,9 @@
+package com.chan.composetryout.compose
+
+import androidx.compose.Composable
+import androidx.ui.foundation.Text
+
+@Composable
+fun inflateAndroidView() {
+    Text(text = "Hello World")
+}
