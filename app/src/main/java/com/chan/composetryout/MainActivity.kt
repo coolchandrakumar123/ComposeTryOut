614918f0ad90c5f1a4e1c07b46c30d774e59dd70
chandra-1765 <chandrakumar.selvaraj@zohocorp.com>
2020-06-09 11:42:06 +0530

    Compose Android View try

--- app/src/main/java/com/chan/composetryout/MainActivity.kt
+++ app/src/main/java/com/chan/composetryout/MainActivity.kt
@@ -1,16 +1,18 @@
 package com.chan.composetryout
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.ui.core.setContent
-import com.chan.composetryout.compose.inflateAndroidView
+import com.chan.composetryout.compose.MarkDownParser
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
+        val context = this
         setContent {
-            inflateAndroidView()
+            //inflateAndroidView()
+            MarkDownParser()
         }
     }
 }
--- app/src/main/java/com/chan/composetryout/compose/AndroidViewScreen.kt
+++ app/src/main/java/com/chan/composetryout/compose/AndroidViewScreen.kt
@@ -1,9 +1,43 @@
 package com.chan.composetryout.compose
 
+import android.content.Context
+import android.util.Log
+import android.webkit.WebView
+import android.widget.TextView
 import androidx.compose.Composable
+import androidx.ui.core.ContextAmbient
 import androidx.ui.foundation.Text
+import androidx.ui.layout.Column
+import androidx.ui.tooling.preview.Preview
+import androidx.ui.viewinterop.AndroidView
 
 @Composable
 fun inflateAndroidView() {
     Text(text = "Hello World")
 }
+
+@Preview(showDecoration = true)
+@Composable
+fun MarkDownParser() {
+    val kotlin = """
+   # Hello World
+   **I'm your boss**
+   ![haha](//hello.com)
+   """.trimIndent()
+    Log.d("ChanLog", "MarkDownParser: Inside")
+    val ctx:Context? = ContextAmbient.current
+    //val html = parseMd(kotlin)
+    Column {
+        Text(text = kotlin)
+        ctx?.let {
+            Log.d("ChanLog", "Context: NonNull")
+            //AndroidView(view = textview(ctx, kotlin))
+            AndroidView(view = webview(ctx, kotlin))
+        }?: kotlin.run {
+            Log.d("ChanLog", "Context: Null")
+        }
+    }
+}
+fun webview(ctx: Context, html: String) = WebView(ctx).apply { loadData(html, "text/html", "utf-8") }
+
+fun textview(ctx: Context, value: String) = TextView(ctx).apply { text = value }
